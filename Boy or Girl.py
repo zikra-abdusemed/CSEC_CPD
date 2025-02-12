fa92ede885b4f27a9fd9327e56400606709a3001
zikra-abdusemed <142779806+zikra-abdusemed@users.noreply.github.com>
2025-02-12 18:24:19 +0300

    Create Boy or Girl.py

---
+++ Boy or Girl.py
@@ -0,0 +1,12 @@
+def main():
+    s1 = input()  
+    s1 = dict.fromkeys(s1)  
+
+    if len(s1) % 2 == 0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
+
+
+if __name__ == "__main__":
+    main()
