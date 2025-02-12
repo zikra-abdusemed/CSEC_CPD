f970155ae94c02ec878e4da69c3311669f857cb7
zikra-abdusemed <142779806+zikra-abdusemed@users.noreply.github.com>
2025-02-12 17:57:11 +0300

    Create Anton and Danik.cpp

---
+++ Anton and Danik.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int t, a = 0, d = 0, j;
+    string str;
+    cin >> t;
+ 
+    cin >> str;
+    for (j = 0; j < str.length(); j++)
+    {
+        if (str[j] == 'A')
+        {
+            a++;
+        }
+        else if (str[j] == 'D')
+        {
+            d++;
+        }
+    }
+    if (a > d)
+    {
+        cout << "Anton" << endl;
+    }
+    else if (d > a)
+    {
+        cout << "Danik" << endl;
+    }
+    else
+    {
+        cout << "Friendship" << endl;
+    }
+}
